747cda95da33388dd423c544d90d8c8c2844df43
tayurus <tayurus@gmail.com>
2018-08-05 16:10:14 +0300

    first version of 'Header' Component

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+
+
+export const Header = (props) => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-7">
+          <h2 className="Header__title">Russian Tourist Visa Support Application</h2>
+        </div>
+        <div className="col-lg-5">
+        </div>
+      </div>
+    </div>
+  )
+}
