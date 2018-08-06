155057ada778d220d5f3f4ae8b971e079008c050
Yury Tatarintsev <yury@realrussialimited.local>
2018-08-06 09:48:01 +0300

    Correct version of prev commit

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -3,7 +3,11 @@ import './Button.css';
 
 export const Button = (props) => {
     let className = typeof props.className !== "undefined" ? props.className : "";
+    let type = typeof props.type !== "undefined" ? props.type : "button";
     return (
-        <button type={props.type} className={"Button " + className} onClick={props.handleClick}>{props.label}</button>
+        <div className="Button-wrapper">
+            <button type={props.type} className={"Button " + className} onClick={props.handleClick}>{props.label}</button>
+            <div className="Button__text">{props.text}</div>
+        </div>
     )
 }
