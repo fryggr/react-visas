1d26b22ec35ada7e7b3aa23c343ce03ad93da684
Yury Tatarintsev <yury@realrussialimited.local>
2018-08-06 10:27:44 +0300

    Update technical task form 'Input' Component. Update 'Input' Component's code for 'type=select'

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,24 +1,47 @@
 import React from "react";
 import "./Input.css";
 
-export const Input = props => {
-  let className = typeof props.className !== "undefined" ? props.className : "";
-  if (props.visited) {
-    className += props.error !== "" ? " incorrect" : " correct";
-  }
+import Select from "react-select";
+import ReactFlagsSelect from "react-flags-select";
+import "react-flags-select/css/react-flags-select.css";
 
-  return (
-    <div className={"Input " + className}>
-      <label className="Input__label">{props.label}</label>
-      <input
-        onBlur={(e) => {props.updateVisited(props.fieldName);props.handleFieldChange(props.fieldName, e.target.value)}}
-        onChange={e => props.handleFieldChange(props.fieldName, e.target.value)}
-        value={props.value}
-        className="Input__field"
-        type={props.type}
-        placeholder={props.placeholder}
-      />
-      <div className="Input__error">{props.error}</div>
-    </div>
-  );
+export const Input = props => {
+    let className = typeof props.className !== "undefined" ? props.className : "";
+    if (props.visited) {
+        className += props.error !== "" ? " incorrect" : " correct";
+    }
+
+    if (props.type !== "select" && props.type !== "date") {
+        return (
+            <div className={"Input " + className}>
+                <label className="Input__label">{props.label}</label>
+                <input
+                    onBlur={e => {
+                        props.updateVisited(props.fieldName);
+                        props.handleFieldChange(props.fieldName, e.target.value);
+                    }}
+                    onChange={e => props.handleFieldChange(props.fieldName, e.target.value)}
+                    value={props.value}
+                    className="Input__field"
+                    type={props.type}
+                    placeholder={props.placeholder}
+                />
+                <div className="Input__error">{props.error}</div>
+            </div>
+        );
+    }
+    else if (props.type === "select"){
+        return (
+            <div className={"Input Select" + className}>
+                <label className="Input__label">{props.label}</label>
+                <Select
+                    className="Input__field"
+                    value={props.currency}
+                    onChange={props.changeCurrency}
+                    options={props.options}
+                />
+                <div className="Input__error">{props.error}</div>
+            </div>
+        )
+    }
 };
